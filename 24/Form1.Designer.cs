84d2ac0afa3253fe0f008657114030f3b49f9f7d
Janne Koponen <janne.koponen@savonia.fi>
2022-02-02 16:54:49 +0200

    Assignment 24 included

---
+++ 24/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace _24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textboxGuess = new System.Windows.Forms.TextBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Guess:";
+            // 
+            // textboxGuess
+            // 
+            this.textboxGuess.Location = new System.Drawing.Point(89, 13);
+            this.textboxGuess.Name = "textboxGuess";
+            this.textboxGuess.Size = new System.Drawing.Size(194, 31);
+            this.textboxGuess.TabIndex = 1;
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(289, 3);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(165, 53);
+            this.buttonSubmit.TabIndex = 2;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(13, 112);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(0, 25);
+            this.labelMessage.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.textboxGuess);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textboxGuess;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.Label labelMessage;
+    }
+}
+
---
+++ 24/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24
+{
+    public partial class Form1 : Form
+    {
+        static Random rnd=new Random();
+        static int number = rnd.Next(0, 101);
+        static int guessCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = Convert.ToString(number);
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int guess = Convert.ToInt32(textboxGuess.Text);
+                guessCount++;
+                if (guess>number)
+                {
+                    labelMessage.Text = "Too big!";
+                } 
+                else if(guess<number)
+                {
+                    labelMessage.Text = "Too small!";
+                }
+                else
+                {
+                    labelMessage.Text = $"Congratulations, you guessed it by {guessCount}! Try again...";
+                    number = rnd.Next(0, 101);
+                    guessCount = 0;
+                    this.Text = Convert.ToString(number);
+                }
+                
+                if(guessCount>7)
+                {
+                    labelMessage.Text = $"You did not guess the easy one, {number}! Try again...";
+                    number = rnd.Next(0, 101);
+                    guessCount = 0;
+                    this.Text = Convert.ToString(number);
+                }
+            }
+            catch(Exception ex)
+            {
+                labelMessage.Text = "Write an integer in range 0...100!";
+            }
+        }
+    }
+}
